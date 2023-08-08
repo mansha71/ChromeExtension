8f69c1d8a8825d18195063ece40946fdc5c16a28
Manan Sharma <mansharm71@gmail.com>
2023-08-07 23:44:12 -0400

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./app.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const [habbits, setHabits] = useState([]);
+
+  return (
+    <div className="app">
+      <h1>Hello World</h1>
+      <h3>Enter New Habbit:</h3>
+      <form action="taskInput">
+        <label htmlFor="taskInput"></label>
+        <input type="text" name="" id="taskentry" />
+        <button className="btn" type="submit">
+          Add
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
