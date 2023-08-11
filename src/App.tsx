0ebdeadd0e18e6f2c8499fa3342d0ec33a9d4da0
Manan Sharma <mansharm71@gmail.com>
2023-08-11 18:48:05 -0400

    added logo

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,37 @@ function App() {
     Array(habits.length).fill(0)
   );
 
+  const currentDate = new Date();
+  const daysInMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + 1,
+    0
+  ).getDate();
+  let diffMonth = false;
+
   console.log(checkboxStatus);
+
+  useEffect(() => {
+    diffMonth = false;
+    const storedMonth = localStorage.getItem("currentMonth");
+    const currentMonth = currentDate.getMonth();
+
+    if (storedMonth && parseInt(storedMonth) !== currentMonth) {
+      // Month has changed, update the stored month and continue streaks
+      diffMonth = true;
+      localStorage.setItem("currentMonth", currentMonth.toString());
+
+      for (let i = 0; i <= habitInput.length; i++) {
+        handleResetTask(i);
+      }
+      setStreaks((prevState) => {
+        const newStreaks = prevState.map((streak) =>
+          streak > 0 ? streak + 1 : 0
+        );
+        return newStreaks;
+      });
+    }
+  }, [currentDate]);
 
   //because useEffect has empty [] at the end, it exectues once when component is first shown
   useEffect(() => {
@@ -55,7 +85,7 @@ function App() {
     if (habitInput.trim() !== "") {
       setHabits([...habits, habitInput.trim()]);
       // Initialize a new checkbox status array with false values for each day
-      const newCheckboxStatus = new Array(31).fill(false);
+      const newCheckboxStatus = new Array(daysInMonth).fill(false);
 
       // Append the new checkbox status array to the existing checkboxStatus array
       setCheckboxStatus([...checkboxStatus, newCheckboxStatus]);
@@ -74,6 +104,9 @@ function App() {
     setStreaks((prevState) => {
       const newState = [...prevState];
       let count = 0;
+      if (diffMonth) {
+        count = newState[index];
+      }
 
       // Starting from the clicked checkbox's previous day
       for (let i = number - 1; i >= 0; i--) {
@@ -110,47 +143,62 @@ function App() {
       }
       return updatedCheckboxStatus;
     });
+
+    setStreaks((prevState) => {
+      const newState = [...prevState];
+      newState[index] = 0;
+      return newState;
+    });
   };
 
   return (
     <div className="app">
-      <h1>Habit Tracker</h1>
+      <div className="h1div">
+        <h1>HABIT SPARK</h1>
+      </div>
       <h3>Enter New Habit:</h3>
       <form onSubmit={handleAddHabit}>
         <label htmlFor="taskInput"></label>
-        <input type="text" value={habitInput} onChange={handleInputChange} />
-        <button className="btn" type="submit">
+        <input
+          type="text"
+          placeholder="Task"
+          value={habitInput}
+          onChange={handleInputChange}
+        />
+        <button className="add-button" type="submit">
           Add
         </button>
       </form>
-      <div>
+      <div className="maindiv">
         {habits.map((habit, index) => (
           <div key={index} className="habit-row">
             <div className="habitName">{habit} :</div>
             <div className="streak">Streak: {streaks[index]}</div>
             <div className="number-checkbox-container">
-              {Array.from({ length: 31 }, (_, i) => i + 1).map((number) => (
-                <div key={number} className="number-checkbox">
-                  <label htmlFor={`habit-${index}-checkbox-${number}`}>
-                    {number}
-                  </label>
-                  <input
-                    type="checkbox"
-                    id={`habit-${index}-checkbox-${number}`}
-                    checked={checkboxStatus[index][number - 1]}
-                    onChange={() => handleCheckboxChange(index, number - 1)}
-                  />
-                </div>
-              ))}
+              {Array.from({ length: daysInMonth }, (_, i) => i + 1).map(
+                (number) => (
+                  <div key={number} className="number-checkbox">
+                    <label htmlFor={`habit-${index}-checkbox-${number}`}>
+                      {number}
+                    </label>
+                    <input
+                      type="checkbox"
+                      id={`habit-${index}-checkbox-${number}`}
+                      checked={checkboxStatus[index][number - 1]}
+                      onChange={() => handleCheckboxChange(index, number - 1)}
+                    />
+                  </div>
+                )
+              )}
             </div>
             <button
-              className="btn btn-danger"
+              className="other-button"
               onClick={() => handleDeleteTask(index)}
             >
               Delete
             </button>
             <button
-              className="btn btn-danger"
+              className="other-button"
               onClick={() => handleResetTask(index)}
             >
               Reset
